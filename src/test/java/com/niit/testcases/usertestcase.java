07bf782a8ae36fa88f3132888fc83beec9489cfb
sunandaverma <sunandaverma30@gmail.com>
2018-05-01 15:22:24 +0530

    renovatebackend connected

---
+++ src/test/java/com/niit/testcases/usertestcase.java
@@ -0,0 +1,14 @@
+package com.niit.testcases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class usertestcase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
